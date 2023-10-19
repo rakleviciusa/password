3b982fbcc979b319579e2b6d790411c182108dfc
rakleviciusa <107745861+rakleviciusa@users.noreply.github.com>
2023-10-20 01:04:30 +0300

    Update password encrypter.cpp

--- password encrypter/password encrypter.cpp
+++ password encrypter/password encrypter.cpp
@@ -1,31 +1,71 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
+#include <cctype>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
-int main()
-{
-    char alphabet[] = { 'A', 'Ą', 'B', 'C', 'Č', 'D', 'E', 'Ę', 'Ė', 'F', 'G', 'H', 'I', 'Į', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'Š', 'T', 'U', 'Ų', 'Ū', 'V', 'Y', 'Z' };
-    string inputPassword;
-    string inputKey;
+void toUpperCase(std::string& password) {
+	for (char& c : password) {
+		c = std::toupper(c);
+	}
+}
 
-    int alphabetLength = sizeof(alphabet) / sizeof(alphabet[0]);
+void repeatKey(string& inputKey, int passwordLength) {
+	while (inputKey.size() < passwordLength) {
+		inputKey += inputKey;
+	}
+	inputKey = inputKey.substr(0, passwordLength);  // Trim to match password length
+}
 
-    cout << "Iveskite pradini teksta" << endl;
-    cin >> inputPassword;
+void passwordToIndexes(string& inputPassword, char* alphabet, int* passwordIndexes, int alphabetLength) {
+	for (int i = 0; i < alphabetLength; i++) {
+		for (int k = 0; k < inputPassword.size(); k++) {
+			if (alphabet[i] == inputPassword[k]) {
+				passwordIndexes[k] = i;
+			}
+		}
+	}
+}
 
-    cout << "Iveskite slapta rakta" << endl;
-    cin >> inputKey;
+int main() {
+	char alphabet[] = { 'A', 'Ą', 'B', 'C', 'Č', 'D', 'E', 'Ę', 'Ė', 'F', 'G', 'H', 'I', 'Į', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'Ų', 'Ū', 'V', 'Z', 'Ž'};
 
-    const int inputPasswordLength = inputPassword.length();
-    char* passwordArray = new char[inputPasswordLength + 1];
-    strcpy(passwordArray, inputPassword.c_str());
+	string inputPassword;
+	string inputKey;
 
-    for (int i = 0; i < inputPasswordLength; i++)
-    {
-        std::cout << passwordArray[i];
-    }
+	int passwordIndexes[50];
+	int keyIndexes[50];
+	int codedPasswordIndexes[50];
 
+	int alphabetLength = sizeof(alphabet);
 
-    
+	cout << "Iveskite pradini teksta" << endl;
+	cin >> inputPassword;
+
+	cout << "Iveskite slapta rakta" << endl;
+	cin >> inputKey;
+
+	toUpperCase(inputPassword);
+	toUpperCase(inputKey);
+
+	repeatKey(inputKey, inputPassword.size());
+
+	passwordToIndexes(inputPassword, alphabet, passwordIndexes, alphabetLength);
+
+	passwordToIndexes(inputKey, alphabet, keyIndexes, alphabetLength);
+
+	for (int i = 0; i < inputPassword.size(); i++) {
+		codedPasswordIndexes[i] = (passwordIndexes[i] + keyIndexes[i]) % alphabetLength;
+	}
+
+	cout << "Coded Password Indexes: ";
+	for (int i = 0; i < inputPassword.size(); i++) {
+		cout << codedPasswordIndexes[i] << " ";
+	}
+
+	return 0;
+
 }
